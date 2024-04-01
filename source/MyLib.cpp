b518be837b6e6084ac6894c0976d214cc1bceef5
Green-Pentagon <115361212+Green-Pentagon@users.noreply.github.com>
2024-04-01 15:17:40 +0100

    removed redundant glm dependencies in MyLib
    removed glm::normalize and glm::cross dependencies and replaced them with the MyLib equivalent functions.

--- source/MyLib.cpp
+++ source/MyLib.cpp
@@ -147,9 +147,9 @@ glm::mat4 MyLib::LookAt(glm::vec3 position, glm::vec3 target, glm::vec3 worldUp)
 	//glm::vec3 target = glm::vec3(0.0f, 0.0f, -4.0f);
 	//glm::vec3 worldUp = glm::vec3(0.0f, 1.0f, 0.0f);
 
-	glm::vec3 cameraFront = glm::normalize(target - position);
-	glm::vec3 cameraRight = glm::normalize(glm::cross(cameraFront, worldUp));
-	glm::vec3 cameraUp = glm::normalize(glm::cross(cameraRight, cameraFront));
+	glm::vec3 cameraFront = MyLib::Normalise(target - position);
+	glm::vec3 cameraRight = MyLib::Normalise(MyLib::Cross(cameraFront, worldUp));
+	glm::vec3 cameraUp = MyLib::Normalise(MyLib::Cross(cameraRight, cameraFront));
 
 	glm::mat4 align = glm::mat4(1.0f);
 
